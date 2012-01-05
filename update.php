90444f470d05e822d9c0f80baea364423ba5a33d
Yao Wei <mwei@lxde.org>
2012-01-05 16:57:50 +0800

    SQL INJECTION FIX

--- update.php
+++ update.php
@@ -18,18 +18,18 @@
 
 
     if ($_POST["submit"]=='修改機器' || $_POST["submit"]=='加入機器') {
-        $ip = $_POST["IP_last_4_digits"];
-        $feature = $_POST["machine_feature"];
-        $ports = $_POST["machine_ports"];
-        $owner = $_POST["machine_owner"];
-        $location = $_POST["machine_location"];
+        $ip = mysql_real_escape_string($_POST["IP_last_4_digits"]);
+        $feature = mysql_real_escape_string($_POST["machine_feature"]);
+        $ports = mysql_real_escape_string($_POST["machine_ports"]);
+        $owner = mysql_real_escape_string($_POST["machine_owner"]);
+        $location = mysql_real_escape_string($_POST["machine_location"]);
 
         $query = 'UPDATE ips SET used=1,func="'.$feature.'",ports="'.$ports.'",owner="'.$owner.'",place="'.$location.'" WHERE ip="'.$ip.'"';
     }
 
     if($_POST["submit"]=='清除機器')
     {
-        $ip = $_POST["IP_last_4_digits"];
+        $ip = mysql_real_escape_string($_POST["IP_last_4_digits"]);
 	$query = 'UPDATE ips SET used=0 WHERE ip="'.$ip.'"';
     }
 
@@ -37,11 +37,11 @@
     {
         if(check())
 	{
-		$newName = $_POST["new_account_name"];
-		$newPasswd = $_POST["new_account_password"];
-		$newPasswdAgain = $_POST["new_account_password_check"];
-		$newMail = $_POST["new_account_mail"];
-    		$newPhone = $_POST["new_account_Phone"];
+		$newName = mysql_real_escape_string($_POST["new_account_name"]);
+		$newPasswd = mysql_real_escape_string($_POST["new_account_password"]);
+		$newPasswdAgain = mysql_real_escape_string($_POST["new_account_password_check"]);
+		$newMail = mysql_real_escape_string($_POST["new_account_mail"]);
+    		$newPhone = mysql_real_escape_string($_POST["new_account_Phone"]);
 		if($newPasswd == $newPasswdAgain)
 		    $query = 'INSERT INTO users VALUES ("'.$newName.'","'.$newPasswd.'","'.$newMail.'","'.$newPhone.'")';
 	}
@@ -51,14 +51,14 @@
     {
         if(check())
 	{
-		$newName = $_POST["new_account_name"];
+		$newName = mysql_real_escape_string($_POST["new_account_name"]);
 		$query = 'DELETE FROM users WHERE name="'.$newName.'"';
 	}
     }
 
     if($_POST["submit"]=='清除機器')
     {
-        $ip = $_POST["IP_last_4_digits"];
+        $ip = mysql_real_escape_string($_POST["IP_last_4_digits"]);
 	$query = 'UPDATE ips SET used=0 WHERE ip="'.$ip.'"';
     }
 
@@ -74,8 +74,8 @@
 function check(){
     if (!isSuperUser()) return false;
 
-    $userName = $_POST["account_name"];
-    $userPasswd = $_POST["account_password"];
+    $userName = mysql_real_escape_string($_POST["account_name"]);
+    $userPasswd = mysql_real_escape_string($_POST["account_password"]);
     
     $query = 'SELECT passwd FROM users WHERE name="'.$userName.'"';
     $result = mysql_query($query);
