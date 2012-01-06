9f03e3e96af15b170a260a54fe90814ced6f38ae
Yao Wei (魏銘廷) <mwei@lxde.org>
2012-01-06 11:59:39 +0800

    try to fix mysql bug

--- check.php
+++ check.php
@@ -8,11 +8,11 @@
         echo "</script>";
     }
     
-    $userName = mysql_real_escape_string($_POST["account_name"]);
-    $userPasswd = mysql_real_escape_string($_POST["account_password"]);
-
     $link = mysql_connect("localhost","iper","ipDBuse") or die("無法與MySQL建立連線");
     mysql_select_db("iptable");
+
+    $userName = mysql_real_escape_string($_POST["account_name"]);
+    $userPasswd = mysql_real_escape_string($_POST["account_password"]);
     
     $query = "SELECT * FROM users WHERE name=\"$userName\" AND passwd=\"$userPasswd\"";
     $result = mysql_query($query);
