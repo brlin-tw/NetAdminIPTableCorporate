505fa2f558550826067562bae1c5bb45f2615475
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-01-08 05:15:03 +0800

    改良訊息

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
       國立海洋大學資訊工程學系 B97570146 楊力維、09957010 林博仁、Ming-Ting Wei<br />
       如有授權需求歡迎<a href='mailto:pika1021@gmail.com'>來信洽詢</a> <br />
       專案說明簡報: <a href='meta/Manual/Project_manual.odp'>Open Document格式</a> | <a href='meta/Manual/Project_manual.pdf'>PDF格式</a><br />
-      「適用於網路管理者的 IP 位址表」為開放原始碼專案，歡迎任何人至<a href='https://github.com/Vdragon/NetAdminIPTableCorporate/' target='_blank'>github</a>獲取全部的原始程式碼、修改、重新發佈、開立issue或是參與這個專案，讓它變得更加完美！
+      「適用於網路管理者的 IP 位址表」為開放原始碼專案，歡迎任何人至<a href='https://github.com/Vdragon/NetAdminIPTableCorporate/' target='_blank'>github</a>獲取全部的原始程式碼、修改、重新發佈或是參與這個專案（包括但不限於加入程式開發、回報問題、提供建議、美化介面等等），讓它變得更加完美！
     </p>
   </div>
 </div>
