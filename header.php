577445333b15b67a371b0be332ca52169ef41922
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-01-08 02:31:50 +0800

    Try to fix index.php HTML validation

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php 
+print ("<?xml version='1.0' encoding='utf-8'?>".PHP_EOL);
 include_once ("functions.php");
-print ('<?xml version="1.0" encoding="utf-8"?>'); ?>
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh_TW" lang="zh_TW">
