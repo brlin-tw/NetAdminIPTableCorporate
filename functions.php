c68edda2b3e5f59a53996e4330ed58ac51b80347
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-01-08 01:13:21 +0800

    Try to fix conflict

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 /* include constants */
@@ -24,7 +24,7 @@ function isSuperUser($username = null) {
 function user_account_check($username, $password, $haveLink = null){
     /* create a link if there's no one */
     if ($haveLink == null) {
-        $link = mysql_connect(MYSQL_LOCATION, MYSQL_USERNAME, MYSQL_PASSWORD) or die("無法與MySQL建立連線");
+        $link = mysql_connect(MYSQL_LOCATION, MYSQL_USERNAME, MYSQL_PASSWORD) or die("您瀏覽的網頁因為「無法與MySQL伺服器建立連線」原因無法正常運作，請稍候再試，如果仍無法正常運作請聯絡網站管理人員處理。");
     } else {
         $link = $haveLink;
     }
@@ -33,11 +33,7 @@ function user_account_check($username, $password, $haveLink = null){
     $password_escaped = mysql_real_escape_string($password);
     $query = "SELECT passwd FROM users WHERE name=\"$username_escaped\" AND passwd=\"$password_escaped\"";
 
-<<<<<<< HEAD
-    $link = mysql_connect(MYSQL_LOCATION, MYSQL_USERNAME, MYSQL_PASSWORD) or die("無法與MySQL伺服器建立連線，請稍候再試，如果仍無法正常運作請聯絡網站管理人員處理。");
-=======
->>>>>>> 208e4ec951b5e6e6c206564d152bca6ae3d64e35
-    mysql_select_db(MYSQL_DATABASE); 
+    mysql_select_db(MYSQL_DATABASE);
     $result = mysql_query($query);
 
     if ($result && mysql_num_rows($result) > 0) {
