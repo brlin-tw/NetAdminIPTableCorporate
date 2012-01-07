60f57d62c1e1d4f71de60d8268f6afc6c619fd0a
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-01-08 02:35:31 +0800

    更改footer

--- footer.php
+++ footer.php
@@ -5,9 +5,9 @@
       <a href="http://jigsaw.w3.org/css-validator/check/referer" target='_blank'><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="W3C CSS Validation Service 驗證通過圖示 | W3C CSS Validater Service Verification Passed Icon" /></a>
     </p>
     <p>
-      國立海洋大學資訊工程學系 B97570146 楊力維、09957010 林博仁 <br />
+      國立海洋大學資訊工程學系 B97570146 楊力維、09957010 林博仁、Ming-Ting Wei<br />
       如有授權需求歡迎<a href='mailto:pika1021@gmail.com'>來信洽詢</a> <br />
-      Project 說明簡報: <a href='meta/Manual/Project_manual.odp'>Open Document格式</a> <a href='meta/Manual/Project_manual.pdf'>PDF格式</a>
+      Project 說明簡報: <a href='meta/Manual/Project_manual.odp'>Open Document格式</a> | <a href='meta/Manual/Project_manual.pdf'>PDF格式</a>
     </p>
   </div>
 </div>
