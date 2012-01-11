c8af3054b3e442acd12b538ce659428bf58d12e9
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-01-11 21:17:23 +0800

    footer添加內容

--- footer.php
+++ footer.php
@@ -5,9 +5,10 @@
       <a href="http://jigsaw.w3.org/css-validator/check/referer" target='_blank'><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="W3C CSS Validation Service 驗證通過圖示 | W3C CSS Validater Service Verification Passed Icon" /></a>
     </p>
     <p>
-      國立海洋大學資訊工程學系 B97570146 楊力維、09957010 林博仁、Ming-Ting Wei<br />
-      如有授權需求歡迎<a href='mailto:pika1021@gmail.com'>來信洽詢</a> <br />
-      專案說明簡報: <a href='meta/Manual/Project_manual.odp'>Open Document格式</a> | <a href='meta/Manual/Project_manual.pdf'>PDF格式</a><br />
+      「適用於網路管理者的 IP 位址表」專案詳細資訊：<br />
+      專案原始開發者：國立海洋大學資訊工程學系 B97570146 楊力維、09957010 林博仁、B97570013 魏銘廷<br />
+      如有任何問題歡迎<a href='mailto:pika1021@gmail.com'>來信指教</a><br />
+      專案說明文件：<a href='meta/Manual/Project_manual.odp'>Open Document格式</a>（Open Document格式文件可以透過可免費下載使用的<a href='http://www.libreoffice.org/' target='_blank'>LibreOffice文書處理軟體</a>檢視以及編輯） | <a href='meta/Manual/Project_manual.pdf'>PDF格式</a><br />
       「適用於網路管理者的 IP 位址表」為開放原始碼專案，歡迎任何人至<a href='https://github.com/Vdragon/NetAdminIPTableCorporate/' target='_blank'>github</a>獲取全部的原始程式碼、修改、重新發佈或是參與這個專案（包括但不限於加入程式開發、回報問題、提供建議、美化介面等等），讓它變得更加完美！
     </p>
   </div>
