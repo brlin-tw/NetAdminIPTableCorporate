59374bfcfecbe50706e006746a7bd2ad1a57cfeb
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-01-09 08:23:46 +0800

    [fix.php]標題變數化、修正文字

--- fix.php
+++ fix.php
@@ -1,15 +1,15 @@
 <?php
   include_once ("functions.php");
 
-  $title = "首頁";
+  $title = "修改留言";
   $page = "home";
   include ("header.php");
   extract($_GET);
 ?>
     
 	</div>
-		<h1 id="heading">~~~開始留言~~~</h1>
-		<p>~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</p>
+		<h1 id="heading"><?php echo $title; ?></h1>
+		<hr />
 		<div id='a'>
 			<form method="POST" action="do_test.php">
 				<table>
@@ -21,7 +21,7 @@
 							<td><input type = "hidden" id="time" name = 'time' value = '$time'/></td>
 						</tr>
 						<tr>
-							<td>留言:<input type = "text" id="ins" name='ins' size = "100" maxlength = "100" /></td>
+							<td>留言：<input type = "text" id="ins" name='ins' size = "100" maxlength = "100" /></td>
 							<td><input type = "submit" value='送出' /></td>
 						</tr>
 					<?php }else{?>
@@ -32,11 +32,12 @@
 						<td><input type = "hidden" id="time" name = 'time' value = '<?PHP echo $time; ?>'/></td>
 					</tr>
 					<tr>
-						<td>留言:<input type = "text" id="ins" name='ins' size = "100" maxlength = "100" /></td>
+						<td>留言：<input type = "text" id="ins" name='ins' size = "100" maxlength = "100" /></td>
 						<td><input type = "submit" value='送出' /></td>
 					</tr>
 					<?php } ?>
 				</table>
 			</form>
 		</div>
+	</div>
 <?php include ("footer.php"); ?>
